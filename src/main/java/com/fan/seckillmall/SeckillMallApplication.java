7bfaf7d11a26ad023956b0bbf40dd2f6b45ebf63
killerman <317309516@qq.com>
2020-01-14 10:07:21 +0800

    init the springboot project

---
+++ src/main/java/com/fan/seckillmall/SeckillMallApplication.java
@@ -0,0 +1,13 @@
+package com.fan.seckillmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeckillMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fan/seckillmall/SeckillMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fan.seckillmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeckillMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
